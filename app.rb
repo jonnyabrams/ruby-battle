5f361a112004a0b8e748d921263df1ec83fa4d9e
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-09 16:47:08 +0000

    feat: switch to modular style

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
-require "sinatra"
-require "sinatra/reloader" if development?
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BattleApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
-    erb(:index)
+    erb :index
   end
-  
-  get '/cat' do
-    
-  end
+
+  # # Start the server if this file is executed directly (do not change the line below)
+  run! if app_file == $0
+end
