23132b05919deea6896ea714748419556888e3d9
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-09 16:40:05 +0000

    feat: index view

--- app.rb
+++ app.rb
@@ -2,11 +2,9 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
   get '/' do
-    "Hello World"
+    erb(:index)
   end
   
   get '/cat' do
-    "<div>
-    <img src='http://placekitten.com/500/500'>
-    </div>"
+    
   end
