2d50fb82d666616bf4c2c63e272a4cc983072cd0
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-09 16:32:21 +0000

    feat: cat route

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
-require 'sinatra'
+require "sinatra"
+require "sinatra/reloader" if development?
 
-get '/' do
-  "Hello World"
-end
-
+  get '/' do
+    "Hello World"
+  end
+  
+  get '/cat' do
+    "<div>
+    <img src='http://placekitten.com/500/500'>
+    </div>"
+  end
